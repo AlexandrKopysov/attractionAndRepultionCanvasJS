6dd8bdbb62526f80a29c5e7c76e496265fa7930f
Alexandr <bayzer47@gmail.comr>
2020-09-01 14:21:33 +0500

    Добавил работу с webp

--- gulpfile.js
+++ gulpfile.js
@@ -14,14 +14,14 @@ let path = {
     html: [source_folder + "/*.html", "!" + source_folder + "/_*.html"],
     css: source_folder + "/scss/style.scss",
     js: source_folder + "/js/script.js",
-    img: source_folder + "/img/**/*.{jpg,png,svg,gif,ico,webp}",
+    img: source_folder + "/img/**/*.{jpg,png,svg,gif,ico,webp,jpeg}",
     fonts: source_folder + "/fonts/*.ttf",
   },
   watch: {
     html: source_folder + "/**/*.html",
     css: source_folder + "/scss/**/*.scss",
     js: source_folder + "/js/**/*.js",
-    img: source_folder + "/img/**/*.{jpg,png,svg,gif,ico,webp}",
+    img: source_folder + "/img/**/*.{jpg,png,svg,gif,ico,webp,jpeg}",
   },
   clean: "./" + project_folder + "/"
 }
@@ -35,8 +35,13 @@ let { src, dest } = require('gulp'),
   autoprefixer = require("gulp-autoprefixer"),
   group_media = require("gulp-group-css-media-queries"),
   clean_css = require("gulp-clean-css"),
-  rename = require("gulp-rename");
-uglify = require("gulp-uglify-es").default;
+  rename = require("gulp-rename"),
+  uglify = require("gulp-uglify-es").default,
+  imagemin = require("gulp-imagemin"),
+  webp = require("gulp-webp"),
+  webphtml = require("gulp-webp-html"),
+  webpcss = require("gulp-webpcss"),
+  svgSprite = require("gulp-svg-sprite");
 
 
 function browserSync(params) {
@@ -54,32 +59,9 @@ function browserSync(params) {
 function html() {
   return src(path.src.html)
     .pipe(fileinclude1())
+    .pipe(webphtml())
     .pipe(dest(path.bild.html))
     .pipe(browsersync.stream())
-}
-
-function js() {
-  return src(path.src.js)
-    .pipe(fileinclude1())
-    .pipe(dest(path.bild.js))
-    .pipe(uglify())
-    .pipe(
-      rename({
-        extname: ".min.js"
-      })
-    )
-    .pipe(dest(path.bild.js))
-    .pipe(browsersync.stream())
-}
-
-function watchFiles(param) {
-  gulp.watch([path.watch.html], html);
-  gulp.watch([path.watch.css], css);
-  gulp.watch([path.watch.js], js);
-}
-
-function clean(params) {
-  return del(path.clean);
 }
 
 function css() {
@@ -89,6 +71,7 @@ function css() {
         outputStyle: "expanded"
       })
     )
+    .pipe(webpcss())
     .pipe(group_media())
     .pipe(
       autoprefixer({
@@ -107,9 +90,72 @@ function css() {
     .pipe(browsersync.stream())
 }
 
-let bild = gulp.series(clean, gulp.parallel(js, css, html));
+function images() {
+  return src(path.src.img)
+    .pipe(
+      webp({
+        quality: 70
+      })
+    )
+    .pipe(dest(path.bild.img))
+    .pipe(src(path.src.img))
+    .pipe(
+      imagemin({
+        progressive: true,
+        svgoPlugins: [{ removeViewBox: false }],
+        interlaced: true,
+        optimizationLevel: 3
+      })
+    )
+    .pipe(dest(path.bild.img))
+    .pipe(browsersync.stream())
+}
+
+function js() {
+  return src(path.src.js)
+    .pipe(fileinclude1())
+    .pipe(dest(path.bild.js))
+    .pipe(uglify())
+    .pipe(
+      rename({
+        extname: ".min.js"
+      })
+    )
+    .pipe(dest(path.bild.js))
+    .pipe(browsersync.stream())
+}
+
+gulp.task('svgSprite', function(){
+  return gulp.src([source_folder + '/iconsprite/*.svg'])
+  pipe(svgSprite({
+    mode: {
+      stack: {
+        sprite: "../icons/icons/svg",
+        example: true
+      }
+    },
+  }))
+  .pipe(dest(path.build.img))
+})
+
+function watchFiles(param) {
+  gulp.watch([path.watch.html], html);
+  gulp.watch([path.watch.css], css);
+  gulp.watch([path.watch.js], js);
+  gulp.watch([path.watch.img], images);
+}
+
+function clean(params) {
+  return del(path.clean);
+}
+
+
+
+let bild = gulp.series(clean, gulp.parallel(js, css, html, images));
 let watch = gulp.parallel(bild, watchFiles, browserSync);
 
+
+exports.images = images;
 exports.js = js;
 exports.css = css;
 exports.html = html;
